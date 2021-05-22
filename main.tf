7569c6dbfbde0674ce206c0547924942f4bc173c
Sam <chmod777recursively@gmail.com>
2021-05-22 16:34:51 -0700

    iam adds to allow terraform to assume role

--- main.tf
+++ main.tf
@@ -1,24 +1,30 @@
 terraform {
-/*
   backend "s3" {
     bucket         = "shooting-insights-terraform-tfstate"
     key            = "terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "codebuild-dynamodb-terraform-locking"
+    dynamodb_table = "shooting-insights-dynamodb-terraform-locking"
     encrypt        = true
   }
-*/
 }
 
 provider "aws" {
   region  = "us-east-1"
-/*
+  
   assume_role {
-    # Remember to update this account ID to yours
-    role_arn     = "arn:aws:iam::718626770228:role/TerraformAssumedIamRole"
+    role_arn     = "arn:aws:iam::272773485930:role/TerraformAssumedIamRole"
     session_name = "terraform"
   }
-*/
+  
+
+  default_tags {
+   tags = {
+     Terraform   = "true"
+     Environment = "Test"
+     Owner       = "Sam"
+     Project     = "ShootingInsights"
+   }
+ }
 }
 
 ## Build an S3 bucket and DynamoDB for Terraform state and locking
@@ -27,4 +33,6 @@ module "bootstrap" {
   s3_tfstate_bucket                   = "shooting-insights-terraform-tfstate"
   s3_logging_bucket_name              = "shooting-insights-logging-bucket"
   dynamo_db_table_name                = "shooting-insights-dynamodb-terraform-locking"
+  lambda_iam_role_name                = "LambdaIamRole"
+  lambda_iam_role_policy_name         = "LambdaIamRolePolicy"
 }
