2c3bd99c939e7453efaa4987464eb771914c2763
Sam Towne <sam.towne2@gmail.com>
2021-06-01 16:20:05 -0700

    data bucket policy

--- modules/lambda/shooting_insights.py
+++ modules/lambda/shooting_insights.py
@@ -5,7 +5,7 @@ def lambda_handler(event, context):
     bucket_name = "shooting-insights-data"
     file_name = "hello.txt"
     lambda_path = "/tmp/" + file_name
-    s3_path = "/directory/" + file_name
+    s3_path = "/test/" + file_name
 
     s3 = boto3.resource("s3")
     s3.Bucket(bucket_name).put_object(Key=s3_path, Body=json.dumps(event))
