05900238cfe291274594975666c245c35eaadfee
Sam Towne <sam.towne2@gmail.com>
2021-06-03 19:31:30 -0700

    lambda script, load json and store to s3 bucket

--- modules/lambda/shooting_insights.py
+++ modules/lambda/shooting_insights.py
@@ -1,17 +1,22 @@
 import json
 import boto3
+import urllib.parse
 
 def lambda_handler(event, context):
-    bucket_name = "shooting-insights-data"
-    file_name = "hello.txt"
-    lambda_path = "/tmp/" + file_name
-    s3_path = "/test/" + file_name
-
-    s3 = boto3.resource("s3")
-    s3.Bucket(bucket_name).put_object(Key=s3_path, Body=json.dumps(event))
-
     
+    body = json.loads("{}".format(event['body']))
+    
+    bucket_name = "shooting-insights-data"
+    file_name = "george.txt"
+    
+    s3_path = "/test/" + file_name
+    s3 = boto3.resource("s3")
+    
+    s3.Bucket(bucket_name).put_object(Key=s3_path, Body=json.dumps(body))
+
     return {
         'statusCode': 200,
-        'body': json.dumps(event)
+        'body': json.dumps(body)
     }
+
+# JSON is being dumped into static text file (overwritten each time)
