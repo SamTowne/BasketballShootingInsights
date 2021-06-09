bd63412de174146c651be24bb199956bd2a3f9fd
Sam <chmod777recursively@gmail.com>
2021-06-08 22:47:12 -0700

    start athena module

---
+++ modules/athena/athena.tf
@@ -0,0 +1,10 @@
+/*
+
+DDL
+spot1 string, spot2 string, spot3 string, spot4 string, spot5 string, spot6 string, spot7 string, spot8 string, spot9 string, spot10 string, spot11 string, temp, date, time
+
+DB and TABLE
+
+
+
+*/
