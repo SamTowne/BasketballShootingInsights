08e13640d62893a93e0fd1ff668a42dff247623c
Sam <chmod777recursively@gmail.com>
2021-06-28 16:50:10 -0700

    Response
    - major improvments to the response and processing functions
    - add cleanup function to remove temp files after each run
    - updates to lambda role policies for response
    - a results query ID is used to retrieve the athena query result
    - the result must be parsed for use and this piece is still in progress

--- modules/lambda/response/response.py
+++ modules/lambda/response/response.py
@@ -2,44 +2,66 @@ import boto3
 import json
 
 def lambda_handler(event, context):
-    ### Send an email using Simple Email Service ###
+
+    ### Retrieve the shooting drill file ###
+    s3 = boto3.resource("s3")
+    shooting_insights_data_bucket = s3.Bucket('shooting-insights-data')
+    processed_temp_file = []
     
+    for obj in shooting_insights_data_bucket.objects.filter(Prefix="temp/3point/"):
+      processed_temp_file = json.loads(obj.get()['Body'].read().decode('utf-8'))
+
+    shots_made          = str(processed_temp_file['shots_made'])
+    shots_attempted     = str(processed_temp_file['shots_attempted'])
+    shooting_percentage = str(processed_temp_file['shooting_percentage'])
+    temp                = str(processed_temp_file['temp'])
+
+    ### Retrieve the Athena Results
+    athena_client = boto3.client('athena')
+    total_made_each_spot_query_results = athena_client.get_query_results(QueryExecutionId=processed_temp_file['total_made_each_spot_athena_execution_id'])
+    data_list = total_made_each_spot_query_results[1].values
+    total_shooting_drills = data_list[0]
+
+    total_made_each_spot = total_shooting_drills 
+        
+    ### Send an email using Simple Email Service ###
+
     SENDER = "Sender Name <chmod777recursively@gmail.com>"
     RECIPIENT = "chmod777recursively@gmail.com"
     AWS_REGION = "us-east-1"
-    # SUBJECT = "3 Point Shooting Drill"
+    #SUBJECT = "3 Point Shooting Drill"
     SUBJECT = "TEST"
 
+
     # The email body for recipients with non-HTML email clients.
-    BODY_TEXT = "Dear Samuel, You made " + "not implemented " + "shots out of " + "not implemented " + "." + "\r\n" + "The data from this shooting drill was stored to an AWS S3 Bucket."
-    BODY_HTML = "test html body"
-    # # The HTML body of the email.
-    # BODY_HTML = """<html>
-    # <head></head>
-    # <body>
-    #   <h1>Shooting Insights</h1>
-    #   <br>
-    #   <p>Dear Samuel,</p>
-    #   <br>
-    #   <p>You made <b>{shots} shots</b> out of {attempted}.</p>
-    #   <p>Your shooting percentage was <b>{shot_perc}%</b>.</p>
-    #   <p>The temperature was <b>{temperature}&deg;F</b>.</p>
-    #   <br>
-    #   <p>The data from this shooting drill was stored to an AWS S3 Bucket:</p>
-    #   <p>{json_body}</p>
-    #   <h4>A serverless app by Sam Towne ¯\_(ツ)_/¯</h4>
-    # </body>
-    # </html>
-    #             """.format(shots=shots_made,attempted=shots_attempted,shot_perc=shooting_percentage,temperature=temp,json_body=body)            
+    BODY_TEXT = "Dear Samuel, You made " + shots_made + " shots out of " + shots_attempted + "." + "\r\n" + "The data from this shooting drill was stored to an AWS S3 Bucket."
+    #BODY_HTML = "test html body " + str(json.loads(processed_temp_file))
+    # The HTML body of the email.
+    BODY_HTML = """<html>
+    <head></head>
+    <body>
+      <h1>Shooting Insights</h1>
+      <br>
+      <p>Dear Samuel,</p>
+      <br>
+      <p>You made <b>{_shots_made} shots</b> out of {_shots_attempted}.</p>
+      <p>Your shooting percentage was <b>{_shooting_percentage}%</b>.</p>
+      <p>The temperature was <b>{_temp}&deg;F</b>.</p>
+      <br>
+      <p>Shooting percentages each location, and totals: {_total_made_each_spot}.</p>
+      <h4>A serverless app by Sam Towne ¯\_(ツ)_/¯</h4>
+    </body>
+    </html>
+                """.format(_shots_made=shots_made,_shots_attempted=shots_attempted,_shooting_percentage=shooting_percentage,_temp=temp,_total_made_each_spot=total_made_each_spot)            
     
     # The character encoding for the email.
     CHARSET = "UTF-8"
     
     # Create a new SES resource and specify a region.
-    client = boto3.client('ses',region_name=AWS_REGION)
+    ses_client = boto3.client('ses',region_name=AWS_REGION)
     
     # Send the email
-    response = client.send_email(
+    send_email_response = ses_client.send_email(
         Destination={
             'ToAddresses': [
                 RECIPIENT,
@@ -63,6 +85,14 @@ def lambda_handler(event, context):
         },
         Source=SENDER,
     )
+
+    ### Invoke cleanup lambda (async)
+
+    lambda_client = boto3.client("lambda")
+    lambda_client.invoke(FunctionName='cleanup',
+                     InvocationType='Event',
+                     LogType='None'
+                     )
 
     return {
         'statusCode': 200,
