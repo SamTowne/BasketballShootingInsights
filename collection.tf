fde2b09db710c3ed800961b0d8f38e60eca2a58f
Sam <chmod777recursively@gmail.com>
2021-06-28 21:33:59 -0700

    Collection Module Restructure
    - Combine resources needed for collection into a single new module
    - Remove many variables that are passed in and hard code them
    - Combined the collection lambda and api gateway resources into single
    module
    - This makes it easier to build and destroy it
    - This is a breaking change
      - The API Gateway route was rebuilt and the form needs to be updated
      - Terraform applies fail because there are dependencies on the old
    modules
    - modules/api_gateway and modules/lambda will be deprecated once all
    modules have been restructured in this way
    - I think this makes the terraform code cleaner
    - Then, the tf root module calls can all be in on main.tf file most
    likely.. or even one for dev and one for prod etc.

--- collection.tf
+++ collection.tf
@@ -6,51 +6,56 @@
 # A lambda function is triggered
 # The lambda function stores the form data to an S3 bucket
 
-module "api_gateway" {
-  source            = "./modules/api_gateway"
-  name              = "shooting_insights"
-  protocol_type     = "HTTP"
-  route_key         = "POST /si/submit"
-  target            = module.collection_lambda.output_arn
-  lambda_arn        = module.collection_lambda.output_arn
-  lambda_invoke_arn = module.collection_lambda.output_invoke_arn
+module "collection" {
+  source            = "./modules/collection"
+  data_bucket_arn     = module.bootstrap.data_bucket_arn
 }
 
-module "collection_lambda" {
-  source              = "./modules/lambda"
-  role                = "collection_lambda_role"
-  filename            = module.collection_lambda.output_path
-  function_name       = "collection"
-  handler             = "collection.lambda_handler"
-  runtime             = "python3.8"
-  source_code_hash    = filebase64sha256(module.collection_lambda.output_path)
+# module "api_gateway" {
+#   source            = "./modules/api_gateway"
+#   name              = "shooting_insights"
+#   protocol_type     = "HTTP"
+#   route_key         = "POST /si/submit"
+#   target            = module.collection_lambda.output_arn
+#   lambda_arn        = module.collection_lambda.output_arn
+#   lambda_invoke_arn = module.collection_lambda.output_invoke_arn
+# }
 
-  lambda_policy_json  = <<EOT
-{
-  "Version": "2012-10-17",
-  "Statement": [
-      {
-          "Effect": "Allow",
-          "Action": [
-              "logs:CreateLogGroup",
-              "logs:CreateLogStream",
-              "logs:PutLogEvents",
-              "ses:SendEmail",
-              "ses:SendRawEmail",
-              "lambda:InvokeFunction"
-          ],
-          "Resource": "*"
-      },
-      {
-          "Effect": "Allow",
-          "Action": [
-              "s3:PutObject"
-          ],
-          "Resource": [
-              "${module.bootstrap.data_bucket_arn}"
-          ]
-      }
-  ]
-}
-EOT
-}
+# module "collection_lambda" {
+#   source              = "./modules/lambda"
+#   role                = "collection_lambda_role"
+#   filename            = module.collection_lambda.output_path
+#   function_name       = "collection"
+#   handler             = "collection.lambda_handler"
+#   runtime             = "python3.8"
+#   source_code_hash    = filebase64sha256(module.collection_lambda.output_path)
+
+#   lambda_policy_json  = <<EOT
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#       {
+#           "Effect": "Allow",
+#           "Action": [
+#               "logs:CreateLogGroup",
+#               "logs:CreateLogStream",
+#               "logs:PutLogEvents",
+#               "ses:SendEmail",
+#               "ses:SendRawEmail",
+#               "lambda:InvokeFunction"
+#           ],
+#           "Resource": "*"
+#       },
+#       {
+#           "Effect": "Allow",
+#           "Action": [
+#               "s3:PutObject"
+#           ],
+#           "Resource": [
+#               "${module.bootstrap.data_bucket_arn}"
+#           ]
+#       }
+#   ]
+# }
+# EOT
+# }
