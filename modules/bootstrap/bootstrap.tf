979027c0aed6c64f8f70efbe12c7605b9f326a86
Sam Towne <sam.towne2@gmail.com>
2021-09-08 11:05:26 -0700

    Data Bucket and Google Provider
    - Add data bucket back into bootstrap module
    - Remove the google provider
    - Update the data bucket IAM policy and lifecycle

--- modules/bootstrap/bootstrap.tf
+++ modules/bootstrap/bootstrap.tf
@@ -42,3 +42,50 @@ resource "aws_dynamodb_table" "tf_lock_state" {
     type = "S"
   }
 }
+
+resource "aws_s3_bucket" "s3_data_bucket" {
+  bucket = var.data_bucket
+  acl    = "private"
+  policy = <<EOT
+  {
+    "Version": "2012-10-17",
+    "Id": "SIDataBucketPolicy",
+    "Statement": [
+        {
+            "Sid": "lambda",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "arn:aws:iam::272773485930:role/collection_lambda_role"
+            },
+            "Action": "s3:PutObject",
+            "Resource": "arn:aws:s3:::shooting-insights-data/*"
+        }
+    ]
+}
+EOT
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  lifecycle_rule {
+    id = "data bucket lifecycle"
+    enabled = true
+    abort_incomplete_multipart_upload_days = 1
+    
+    noncurrent_version_expiration {
+      days = 1
+    }
+  }
+}
+
+output "data_bucket" {
+  value = aws_s3_bucket.s3_data_bucket.bucket
+}
+output "data_bucket_arn" {
+  value = aws_s3_bucket.s3_data_bucket.arn
+} 
